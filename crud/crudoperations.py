58ccabe4272b96d70067985515a0f662290b97ce
Pick-Y <a.scarcella.as@gmail.com>
2024-02-06 07:45:12 +1100

    created a show function

--- crud/crudoperations.py
+++ crud/crudoperations.py
@@ -12,3 +12,16 @@ def add():
     with open(f"./file/" + "todos.txt", 'w') as file:
         #we write the "todo_list" line by line into the new file
         file.writelines(todo_list)
+
+def show():
+    with open(f"./file/" + "todos.txt", 'r') as file:
+        list_of_item = file.readlines()
+    
+    new_item_list = [item.strip("\n") for item in list_of_item]
+    
+    for index, i in enumerate(new_item_list):
+                #we want to enumerate list index starting from one and not 0
+                #for a better UX
+                numbered_item = f"{index + 1}:){i}"
+                #Print the items present in the txt file
+                print(numbered_item)
