6f3061bb3c99aba561085949dec6a0e81a37e9f3
Pick-Y <a.scarcella.as@gmail.com>
2024-02-06 07:16:57 +1100

    Added a while loop and an add case

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import sys
+sys.path.append('crud')
+from crudoperations import add
 
-#Create a "while loop" that gives the user options to:
+
+
+
 
 while True:
     user_selection = input("Choose an option from the list:\n 1) add(to addan item)\n 2) show(to show todo_items in list)\n 4) edit (to edit item in the list)\n 5) complete (to remove an item from the list)\n6) end (to exit program)\n")
 
+    match user_selection.strip():
+        case "add":
+            add()
 # - add a new items to a list
+    
     
 # - show the content of a list
 # - edit the content of a list
