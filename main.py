c3617e7c094ac93656928f116e0bdb72a8fd48f7
Pick-Y <a.scarcella.as@gmail.com>
2024-02-07 01:22:23 +1100

    Fixed bug - an item was added to the list when writing -add- after -editing

--- main.py
+++ main.py
@@ -9,16 +9,19 @@ from crudoperations import add,show,edit, complete
 while True:
     user_selection = input("Choose an option from the list:\n 1) add(to addan item)\n 2) show(to show todo_items in list)\n 4) edit (to edit item in the list)\n 5) complete (to remove an item from the list)\n 6) end (to exit program)\n")
     user_selection = user_selection.strip()
-    if "add" in user_selection:
+
+    if  user_selection.startswith("add"):
         add(user_selection[4:])
-    elif "show" in user_selection.strip():
+    elif user_selection.startswith("show"):
         show()
-    elif "edit" in user_selection.strip():
+    elif user_selection.startswith("edit"):
         edit()
-    elif "complete" in user_selection.strip():
+    elif user_selection.startswith("complete"):
         complete()
-    else:
+    elif user_selection.startswith("end"):
         break
+    else:
+        print("Invalid command")
 
 # - add a new items to a list
 # - show the content of a list
