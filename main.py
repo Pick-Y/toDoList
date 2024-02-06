8270c6d876d1618ee348f0211f9340f9a6e5f97e
Pick-Y <a.scarcella.as@gmail.com>
2024-02-06 20:15:10 +1100

    replaced case with if statements

--- main.py
+++ main.py
@@ -9,15 +9,16 @@ from crudoperations import add,show,edit, complete
 while True:
     user_selection = input("Choose an option from the list:\n 1) add(to addan item)\n 2) show(to show todo_items in list)\n 4) edit (to edit item in the list)\n 5) complete (to remove an item from the list)\n 6) end (to exit program)\n")
 
-    match user_selection.strip():
-        case "add":
-            add()
-        case "show":
-            show()
-        case "edit":
-            edit()
-        case "complete":
-            complete()
+    if "add" in user_selection.strip():
+        add()
+    elif "show" in user_selection.strip():
+        show()
+    elif "edit" in user_selection.strip():
+        edit()
+    elif "complete" in user_selection.strip():
+        complete()
+    else:
+        break
 
 # - add a new items to a list
 # - show the content of a list
