53de60aad14d2f4cfd51476a14990d023f137bc0
Pick-Y <a.scarcella.as@gmail.com>
2024-02-12 23:20:26 +1100

    refactored programi logic. imported TodoListApp

--- main.py
+++ main.py
@@ -1,34 +1,15 @@
 import sys
-sys.path.append('crud')
-from crudoperations import add,show,edit, complete
+#sys.path.append('crud')
+#from crudoperations import add,show,edit, complete
 from datetime import date, datetime
+from PyQt6.QtWidgets import QApplication
+from gui import TodoListApp
 
+app = QApplication(sys.argv)
+new_todo_list = TodoListApp()
 
-today = date.today()
-todays_date = today.strftime("%B %d, %Y")
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-
-print(todays_date + " " + current_time)
-
-while True:
-    user_selection = input("Choose an option from the list:\n 1) add(to addan item)\n 2) show(to show todo_items in list)\n 4) edit (to edit item in the list)\n 5) complete (to remove an item from the list)\n 6) end (to exit program)\n")
-    user_selection = user_selection.strip()
-
-    if  user_selection.startswith("add"):
-        add(user_selection[4:])
-    elif user_selection.startswith("show"):
-        show()
-    elif user_selection.startswith("edit"):
-        edit()
-    elif user_selection.startswith("complete"):
-        complete()
-    elif user_selection.startswith("end"):
-        break
-    else:
-        print("Invalid command")
-
-# - add a new items to a list
-# - show the content of a list
-# - edit the content of a list
-# - remove an item from the list when the task has been completed
+if __name__ == '__main__':
+    
+    window = TodoListApp()
+    window.show()
+    sys.exit(app.exec())
