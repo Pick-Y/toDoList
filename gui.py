711ab006243a08e740ed52d3fd5464358c716d01
Pick-Y <a.scarcella.as@gmail.com>
2024-02-12 22:52:48 +1100

    created TodoListApp class

--- gui.py
+++ gui.py
@@ -1,2 +1,11 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QListWidget, QLineEdit, QPushButton, QMessageBox,QLabel,QDialog
+
+
+
+class TodoListApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.path = "todos.txt"
+        self.list_of_items = self.read_file()
